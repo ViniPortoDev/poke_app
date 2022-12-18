654509cc841b52e9321f77551d7456ad10e72b8f
ViniNormie <vinicius.p.a.corin@gmail.com>
2022-12-18 14:24:28 -0300

    confirguração de temas e estrutura do projeto

---
+++ lib/src/pages/home/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PokeLocHomePage extends StatelessWidget {
+  const PokeLocHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+          child: Text(
+        'Oi',
+        style: TextStyle(
+          fontSize: 30,
+        ),
+      )),
+    );
+  }
+}
